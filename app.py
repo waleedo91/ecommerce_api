e896876ce037d5597c2200c85a93d5cb8664fceb
Waleed <wsaleh1991@gmail.com>
2024-12-18 08:07:07 -0600

    Created relationshional tables between 3 entities. crud operations be created next.

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 ma = Marshmallow(app)
 
+order_product = Table(
+    "order_product",
+    Base.metadata,
+    Column("order_id", ForeignKey("orders.id")),
+    Column("product_id", ForeignKey('products.id'))
+)
+
 
 class User(Base):
     __tablename__ = "users"
@@ -34,6 +41,9 @@ class Order(Base):
     order_date: Mapped[datetime.datetime] = mapped_column(
         DateTime, default=datetime.datetime.now
     )
+    customer: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    products: Mapped[List['Product']] = relationship(
+        secondary=order_product, back_populates='orders')
 
 
 class Product(Base):
@@ -41,6 +51,10 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     product_name: Mapped[str] = mapped_column(String(200))
     price: Mapped[float] = mapped_column()
+
+    orders: Mapped[List["Order"]] = relationship(
+        secondary=order_product, back_populates="products"
+    )
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
@@ -52,9 +66,11 @@ class OrderSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Order
 
+
 class ProductSchema(ma.SQLAlchemyAutoSchema):
-    class Meta: 
+    class Meta:
         model = Product
+
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
@@ -62,6 +78,63 @@ order_schema = OrderSchema()
 orders_schema = OrderSchema(many=True)
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
+
+
+# TODO:  User Endpoints
+
+# GET /users: Retrieve all users
+
+@app.route('/users', method=['GET'])
+def get_users():
+    pass
+
+# GET /users/<id>: Retrieve a user by ID
+
+
+@app.route('/users/<int:user_id>', method=['GET'])
+def get_single_user(user_id):
+    pass
+
+# POST /users: Create a new user
+
+
+@app.route('/users', method=['POST'])
+def create_user():
+    pass
+
+# PUT /users/<id>: Update a user by ID
+
+
+@app.route('/users/<int:user_id>', method=['PUT'])
+def update_user(user_id):
+    pass
+
+# DELETE /users/<id>: Delete a user by ID
+
+
+@app.route('/user/<int:user_id>', method=['DELETE'])
+def delete_user(user_id):
+    pass
+
+
+# ===================================================
+
+# TODO: Product Endpoints
+# GET /products: Retrieve all products
+# GET /products/<id>: Retrieve a product by ID
+# POST /products: Create a new product
+# PUT /products/<id>: Update a product by ID
+# DELETE /products/<id>: Delete a product by ID
+
+# ======================================================
+
+# TODO: Order Endpoints
+# POST /orders: Create a new order (requires user ID and order date)
+# GET /orders/<order_id>/add_product/<product_id>: Add a product to an order (prevent duplicates)
+# DELETE /orders/<order_id>/remove_product: Remove a product from an order
+# GET /orders/user/<user_id>: Get all orders for a user
+# GET /orders/<order_id>/products: Get all products for an order
+
 
 if __name__ == "__main__":
     with app.app_context():
